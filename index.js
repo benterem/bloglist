3e86507b3503ae0c9fd453d4d139b1e976f76cde
Ben Terem <benterem@gmail.com>
2021-06-06 11:57:39 +0300

    index

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
+
+
+/* ------------------------------------------------------------------------------------------------- */
+
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
