fb802d9347c0d0745bab2e9c4dbe59e90f50fd5e
Ben Terem <benterem@gmail.com>
2021-06-06 12:21:33 +0300

    index

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const app = express()
+const app = require('./app')
 const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
@@ -9,18 +9,3 @@ const server = http.createServer(app)
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
-
-
-/* ------------------------------------------------------------------------------------------------- */
-
-// const blogSchema = new mongoose.Schema({
-//   title: String,
-//   author: String,
-//   url: String,
-//   likes: Number
-// })
-
-// const Blog = mongoose.model('Blog', blogSchema)
-
-// const mongoUrl = process.env.MONGODB_URI
-// mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
