494cb74e4a71e10a10f537d5f6af9c899c760892
Ben Terem <benterem@gmail.com>
2021-06-06 12:06:26 +0300

    blogsRouter

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-const express = require('express')
 const cors = require('cors')
 
 const server = http.createServer(app)
@@ -15,38 +14,38 @@ server.listen(config.PORT, () => {
 
 /* ------------------------------------------------------------------------------------------------- */
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+// const blogSchema = new mongoose.Schema({
+//   title: String,
+//   author: String,
+//   url: String,
+//   likes: Number
+// })
 
-const Blog = mongoose.model('Blog', blogSchema)
+// const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+// const mongoUrl = process.env.MONGODB_URI
+// mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
-app.use(express.json())
+// app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+// app.get('/api/blogs', (request, response) => {
+//   Blog
+//     .find({})
+//     .then(blogs => {
+//       response.json(blogs)
+//     })
+// })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+// app.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+//   blog
+//     .save()
+//     .then(result => {
+//       response.status(201).json(result)
+//     })
+// })
 
 const PORT = 3003
 app.listen(PORT, () => {
