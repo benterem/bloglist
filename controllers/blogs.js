ce8e586307ddac1df61e77c9f0b467b4a2627479
Ben Terem <benterem@gmail.com>
2021-06-06 12:07:16 +0300

    blogsRouter

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,8 @@
-const express = require('express')
+// const express = require('express')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-app.use(express.json())
+// app.use(express.json())
 
 blogsRouter.get('/', request, response => {
   Blog
