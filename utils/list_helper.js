5a651c2a634dfc3967d9fe593103d33c1fdc07c6
benterem <benterem@gmail.com>
2021-06-09 15:40:30 +0300

    4-5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,20 @@ const dummy =(blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.reduce((acc, blog) => acc + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    if(blogs.length < 1){
+        return undefined
+    }
+    const { title, author, likes } = blogs.sort((a, b) => b.likes - a.likes)[0]
+    return { title, author, likes }
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
