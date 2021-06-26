ac5b6ce14f3688bb0732620925de3bc04e6cc45c
benterem <benterem@gmail.com>
2021-06-26 17:30:20 -0400

    includes users in blogs with populated fields

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,18 +2,29 @@
 const blogsRouter = require('express').Router()
 const { response } = require('express')
 const { request } = require('../app')
+const { findById } = require('../models/blog')
 const Blog = require('../models/blog')
-
+const User = require('../models/user')
 // app.use(express.json())
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1})
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+
+  const user = await User.findById(body.userId)
+
+  const blog = new Blog({
+    ...body,
+    user: user._id
+  })
+
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
